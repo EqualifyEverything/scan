a7712b23e7134d396a3cd5fceec70d1162375124
Bentley Hensel <bentley.hensel@civicactions.com>
2023-03-27 23:10:34 +0000

    Working Crawl

--- scans/crawl.py
+++ scans/crawl.py
@@ -1,4 +1,6 @@
 import psycopg2
+import uuid
+import re
 from scrapy import Spider, Request
 from scrapy.crawler import CrawlerProcess
 
@@ -24,22 +26,48 @@ class A11ySpider(Spider):
     # Where to Start?
     def start_requests(self):
         for domain in domains:
+            # Generate UUID
+            python_uuid = uuid.uuid4()
+
+            # Insert Domain and UUID
+            cur.execute("INSERT INTO results.crawls (domain, python_uuid) VALUES (%s, %s)", (domain[0], str(python_uuid)))
+
             url = "https://" + domain[0]
-            yield Request(url, callback=self.parse, meta={"domain": domain[0]})
+            yield Request(url, callback=self.parse, meta={"domain": domain[0], "python_uuid": python_uuid})
 
     # How are we parsing this?
     def parse(self, response):
-        domain = response.meta["domain"]
+        domain = response.meta["domain"] # get the domain from meta.domains
+        python_uuid = response.meta["python_uuid"] # get the python_uuid
+        source_url = response.url  # get the source URL
 
         # Get all links from the page
-        links = response.css("a::attr(href)").getall()
+        try:
+            links = response.css("a::attr(href)").getall()
+        except Exception as e:
+            self.logger.warning(f"Failed to parse {response.url} with error: {e}")
+            return
 
         # Insert link to Postgres
         for link in links:
             if link is not None:
-                cur.execute("INSERT INTO staging.urls (url) VALUES (%s)", (link,))
-                conn.commit()
-                self.logger.info(f"Inserted {link} into staging.urls")
+                # filter out urls with "#" and remove trailing "/"
+                link = re.sub(r"#.*$", "", link).rstrip("/")
+                if not re.search(r'tel:|mailto:| ', link):
+                    if not link.startswith("http"):
+                        link = f"https://{domain}{link}"
+                    cur.execute("""
+                        INSERT INTO staging.urls (url, python_uuid, source_url)
+                        VALUES (%s, %s, %s)
+                        ON CONFLICT (url) DO NOTHING
+                    """, (link, str(python_uuid), source_url))
+
+                    conn.commit()
+                    # self.logger.info(f"Inserted {link} into staging.urls")
+
+                    # Follow links to other pages on the same domain
+                    if link.startswith(f"https://{domain}"):
+                        yield Request(link, callback=self.parse, meta={"domain": domain, "python_uuid": python_uuid, "source_url": source_url})
 
 # Crawler Settings
 process = CrawlerProcess(settings={
@@ -47,6 +75,8 @@ process = CrawlerProcess(settings={
     "AUTOTHROTTLE_ENABLED": True,
     "AUTOTHROTTLE_START_DELAY": 1,
     "AUTOTHROTTLE_TARGET_CONCURRENCY": 1,
+    "LOG_LEVEL": "WARNING",
+    "LOG_ENABLED": True
 })
 process.crawl(A11ySpider)
 process.start()
